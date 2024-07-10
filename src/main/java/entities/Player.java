45474d09c0735b9bc02aa76f8bbc786e463bf098
vikash <kumarvikashrocks1997@gmail.com>
2024-07-10 13:52:24 +0530

    creating entities

---
+++ src/main/java/entities/Player.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Player {
+    private int health;
+    private final int strength;
+    private final int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+}
