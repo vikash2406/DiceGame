009944120abfd0ba20e1666d5ec8bd99a1630a58
vikash <kumarvikashrocks1997@gmail.com>
2024-07-10 14:53:34 +0530

    creating user input for both players details.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,27 @@
 import entities.Player;
 
+import java.util.Scanner;
+
 public class Game {
 
     public static void main(String[] args) {
-        Player playerA = new Player(50, 5, 10);
-        Player playerB = new Player(100, 10, 5);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Player A's health:");
+        int healthA = scanner.nextInt();
+        System.out.println("Enter Player A's strength:");
+        int strengthA = scanner.nextInt();
+        System.out.println("Enter Player A's attack:");
+        int attackA = scanner.nextInt();
+
+        System.out.println("Enter Player B's health:");
+        int healthB = scanner.nextInt();
+        System.out.println("Enter Player B's strength:");
+        int strengthB = scanner.nextInt();
+        System.out.println("Enter Player B's attack:");
+        int attackB = scanner.nextInt();
+        Player playerA = new Player(healthA, strengthA, attackA);
+        Player playerB = new Player(healthB, strengthB, attackB);
 
         Arena.gameStart(playerA, playerB);
     }
